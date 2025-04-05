d041d6cc33e6ef4845d8f6db27d13414696bad75
DaviCamp0s <147870910+DaviCamp0s@users.noreply.github.com>
2025-04-05 15:20:16 -0300

    Refactor ChecklistsController: remove unused methods and imports for cleaner code

--- src/modules/checklists/checklists.controller.ts
+++ src/modules/checklists/checklists.controller.ts
@@ -1,26 +1,8 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Delete,
-  Put,
-  ParseIntPipe,
-  NotFoundException,
-  UseGuards,
-} from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { ChecklistsService } from "./checklists.service";
 import { CreateCheckListDto } from "./dtos/create-checklist.dto";
-import { UpdateChecklistDto } from "./dtos/update-checklist.dto";
-import {
-  ApiCreatedResponse,
-  ApiNotFoundResponse,
-  ApiOkResponse,
-  ApiTags,
-} from "@nestjs/swagger";
-import { CheckList, Checklist } from "./entities/checklist.entity";
-import { BaseMessages } from "@shared/enums";
+import { ApiCreatedResponse, ApiTags } from "@nestjs/swagger";
+import { CheckList } from "./entities/checklist.entity";
 import { JwtAuthGuard } from "@shared/guards/jwt-auth.guard";
 
 /**
@@ -47,73 +29,5 @@ export class ChecklistsController {
   @UseGuards(JwtAuthGuard)
   create(@Body() dto: CreateCheckListDto) {
     return this.service.create(dto);
-  }
-
-  /**
-   * Retorna todos os checklists.
-   * @returns Uma lista de checklists.
-   */
-  @Get()
-  @ApiOkResponse({
-    type: [Checklist],
-  })
-  findAll() {
-    return this.service.findAll();
-  }
-
-  /**
-   * Retorna um checklist específico pelo ID.
-   * @param id ID do checklist a ser buscado.
-   * @returns O checklist correspondente ao ID fornecido.
-   * @throws NotFoundException Caso o checklist não seja encontrado.
-   */
-  @Get(":id")
-  @ApiOkResponse({
-    type: [Checklist],
-  })
-  @ApiNotFoundResponse({
-    description: BaseMessages.notFound,
-    type: NotFoundException,
-  })
-  findOne(@Param("id", ParseIntPipe) id: number) {
-    return this.service.findOne(id);
-  }
-
-  /**
-   * Atualiza um checklist existente.
-   * @param id ID do checklist a ser atualizado.
-   * @param dto Dados para atualização do checklist.
-   * @returns O checklist atualizado.
-   * @throws NotFoundException Caso o checklist não seja encontrado.
-   */
-  @Put(":id")
-  @ApiOkResponse({
-    type: Checklist,
-  })
-  @ApiNotFoundResponse({
-    description: BaseMessages.notFound,
-    type: NotFoundException,
-  })
-  update(
-    @Param("id", ParseIntPipe) id: number,
-    @Body() dto: UpdateChecklistDto,
-  ) {
-    return this.service.update(id, dto);
-  }
-
-  /**
-   * Remove um checklist existente.
-   * @param id ID do checklist a ser removido.
-   * @returns Confirmação da remoção.
-   * @throws NotFoundException Caso o checklist não seja encontrado.
-   */
-  @Delete(":id")
-  @ApiOkResponse()
-  @ApiNotFoundResponse({
-    description: BaseMessages.notFound,
-    type: NotFoundException,
-  })
-  remove(@Param("id", ParseIntPipe) id: number) {
-    return this.service.remove(id);
   }
 }
