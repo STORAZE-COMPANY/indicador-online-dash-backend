472903b0f6ac2bc43aa1e6557ba336b4613d4ed1
DaviCamp0s <147870910+DaviCamp0s@users.noreply.github.com>
2025-04-10 08:52:22 -0300

    Add MinIO client for image uploads: implement uploadImage function, define enums and interfaces for bucket and image properties

---
+++ src/api/minioClient/enum/index.ts
@@ -0,0 +1,6 @@
+export enum Buckets {
+  indicadorOnlineImages = "indicador-online-images",
+}
+export enum imagesProps {
+  expirationTimeOneHour = 60 * 60, // 1 hour
+}
---
+++ src/api/minioClient/interface/index.ts
@@ -0,0 +1,4 @@
+export interface UploadImageMinioResponse {
+  url: string;
+  imagePath: string;
+}
